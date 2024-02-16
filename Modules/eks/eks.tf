49e3715fb71f7bc531eabc91ca116249059e4c3c
Randeep Puthanpurakkal <randeep_puthanpurakkal@cargill.com>
2024-02-16 14:35:52 +0530

    Day2 policy changes 2

--- Modules/eks/eks.tf
+++ Modules/eks/eks.tf
@@ -2,15 +2,14 @@ resource "aws_iam_role" "eks" {
     name = "${var.Environment}-${var.eks_name}-eks-cluster"
 
     assume_role_policy =  <<-POLICY
-    {
-        "Version": "2012-10-17",
-        "Statement": [
-        "Principal: {
-            "Service": "eks.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-    }
-    ] 
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Sid": "AssumeRolePolicyeks1",
+    "Effect": "Allow",
+    "Principal": { "Service": "eks.amazonaws.com" },
+    "Action": "sts:AssumeRole"
+  }
 }
 POLICY
 }
