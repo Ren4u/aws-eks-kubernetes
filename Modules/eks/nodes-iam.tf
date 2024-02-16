eae3564b780b05c151119b4d31330e4f05b45ea6
Randeep Puthanpurakkal <randeep_puthanpurakkal@cargill.com>
2024-02-16 14:21:19 +0530

    Revert "making corrections to teh role policy error"
    This reverts commit 73c7c4b271f98ccaa1e9df25f8b3651a415f4f00.

--- Modules/eks/nodes-iam.tf
+++ Modules/eks/nodes-iam.tf
@@ -1,16 +1,15 @@
 resource "aws_iam_role" "nodes" {
     name = "${var.Environment}-${var.eks_name}-eks-nodes"
-    assume_role_policy = <<-POLICY
-{
-        "Version" = "2012-10-17"
-        "Statement" = [{
-            "Action" = "sts:AssumeRole"
-            "Effect" = "Allow"
-            "Principal" = {
-                "Service" = "ec2.amazonaws.com"
+    assume_role_policy = jsondecode({
+        Statement = [{
+            Action = "sts:AssumeRole"
+            Effect = "Allow"
+            Principal = {
+                Service = "ec2.amazonaws.com"
             }
         }]
-POLICY
+        Version = "2012-10-17"
+    }) 
 }
 
 resource "aws_iam_role_policy_attachment" "nodes" {
